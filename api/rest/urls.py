25721dc2ce7be0ad49df51e69ddcd4dfb6c6de69
BenjaminKoshyBiju <bennn455@gmail.com>
2023-07-29 00:56:53 +0530

    REST API Completed
    now going to implement this api in the todo app

--- api/rest/urls.py
+++ api/rest/urls.py
@@ -2,6 +2,10 @@ from .import views
 from django.urls import path
 urlpatterns = [
      path('',views.apiAvailable,name='apiAvailable'),
-    #  path('modify/<str:note_id>/',views.modify.as_view(),name='mod'),
+     path('taskList/',views.apiList,name='apiList'),
+     path('taskDetail/<str:pk>/',views.apiDetail,name='apiDetail'),
+     path('taskCreate/',views.apiCreate,name='apiCreate'),
+     path('taskUpdate/<str:pk>/',views.apiUpdate,name='apiUpdate'),
+       path('taskDelete/<str:pk>/',views.apiDelete,name='apiDelete'),
     
 ]
--- api/rest/views.py
+++ api/rest/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import TaskSerializer
+from .models import Task
 
 # Create your views here.
 @api_view(['GET'])
@@ -15,4 +17,46 @@ def apiAvailable(request):
 
     }
     return Response(api_urls)
+
+@api_view(['GET'])
+def apiList(request):
+    tasks=Task.objects.all()
+    serializer=TaskSerializer(tasks, many=True) 
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def apiDetail(request,pk):
+    tasks=Task.objects.get(id=pk)
+    serializer=TaskSerializer(tasks, many=False) 
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def apiCreate(request):
+    serializer=TaskSerializer(data=request.data)
+
+    if serializer.is_valid():
+     serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def apiUpdate(request,pk):
+    task=Task.objects.get(id=pk)
+    serializer=TaskSerializer(instance=task,data=request.data) 
+
+    if serializer.is_valid():
+     serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def apiDelete(request,pk):
+    task=Task.objects.get(id=pk)
+    task.delete()
+    
+    return Response("Deleted Successfully!")
+
+
 
