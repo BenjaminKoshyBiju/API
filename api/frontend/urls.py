03a9f0f3740079c26309ae8c55f88a7a81fa65e1
BenjaminKoshyBiju <bennn455@gmail.com>
2023-07-31 23:58:15 +0530

    Have to make registration for login to work

--- api/frontend/urls.py
+++ api/frontend/urls.py
@@ -2,5 +2,6 @@ from .import views
 from django.urls import path
 urlpatterns = [
      path('',views.list,name='list'),
+     path('login/', views.login_view, name='login'),
     
 ]
--- api/frontend/views.py
+++ api/frontend/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def list(request):
     return render(request,'index.html')
+
+
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('list')  
+        else:
+            # Handle invalid login credentials
+            return render(request, 'login.html', {'error_message': 'Invalid credentials'})
+    return render(request, 'login.html')
+
+
